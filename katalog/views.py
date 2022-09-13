9bcc0976653792ada7a2cc32da2619a301c10c58
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-09-13 14:47:20 +0700

    First Commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Vania Azria Wardani',
+    'id': '2106650380'
+    }
+
+    return render(request, "katalog.html", context)
