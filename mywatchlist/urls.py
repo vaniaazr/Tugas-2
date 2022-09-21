05860fd40b56e425e8c7f8b923d3f605e5ee7e10
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-09-21 17:16:10 +0700

    Film 8th Commit

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -6,7 +6,8 @@ from mywatchlist.views import show_json
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('html/', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_html'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('', show_mywatchlist, name='show_mywatchlist'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,14 +5,58 @@ from django.core import serializers
 
 def show_mywatchlist(request):
     data_barang_mywatchlist = MyWatchlist.objects.all()
+    
+    watched = 0
+    unwatched = 0
+    text = ""
+
+    for movie in data_barang_mywatchlist:
+        if (movie.watched):
+            watched += 1
+        else:
+            unwatched += 1
+
+    if (watched >= unwatched):
+        text = "Selamat, kamu sudah banyak menonton!"
+    else:
+        text = "Wah, kamu masih sedikit menonton!"
+    
     context = {
     'list_barang': data_barang_mywatchlist,
     'nama': 'Vania Azria Wardani',
-    'id': '2106650380'
+    'id': '2106650380',
+    'text': text
     }
 
     return render(request, "mywatchlist.html", context)
 
+def show_html(request):
+    data_barang_mywatchlist = MyWatchlist.objects.all()
+    
+    watched = 0
+    unwatched = 0
+    text = ""
+
+    for movie in data_barang_mywatchlist:
+        if (movie.watched):
+            watched += 1
+        else:
+            unwatched += 1
+
+    if (watched >= unwatched):
+        text = "Selamat, kamu sudah banyak menonton!"
+    else:
+        text = "Wah, kamu masih sedikit menonton!"
+    
+    context = {
+    'list_barang': data_barang_mywatchlist,
+    'nama': 'Vania Azria Wardani',
+    'id': '2106650380',
+    'text': text
+    }
+
+    return render(request, "mywatchlist.html", context)
+    
 def show_xml(request):
     data = MyWatchlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
