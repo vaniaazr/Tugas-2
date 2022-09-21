a193515ea4a7a92136e94b194cb0983d6fa93a1b
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-09-21 16:49:09 +0700

    Film Seventh Commit

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here
+class MyWatchlistTest(TestCase):
+    def test_html(self):
+        response = self.client.get('/mywatchlist/html', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_xml(self):
+        response = self.client.get('/mywatchlist/xml', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_json(self):
+        response = self.client.get('/mywatchlist/json', follow=True)
+        self.assertEqual(response.status_code, 200)
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -6,7 +6,7 @@ from mywatchlist.views import show_json
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
 ]
