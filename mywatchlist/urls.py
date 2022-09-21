26c3a6201eaa6ddc077747f23d6df4632987d40a
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-09-21 10:07:47 +0700

    Film Second Commit

---
+++ mywatchlist/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_xml
+from mywatchlist.views import show_json
+
+app_name = 'mywatchlist'
+
+urlpatterns = [
+    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from mywatchlist.models import MyWatchlist
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+def show_mywatchlist(request):
+    data_barang_mywatchlist = MyWatchlist.objects.all()
+    context = {
+    'list_barang': data_barang_mywatchlist,
+    'nama': 'Vania Azria Wardani',
+    'id': '2106650380'
+    }
+
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
