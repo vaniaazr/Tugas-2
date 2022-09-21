c66df861101af8fe703d34cdd1f08e5e59c25226
Vania Azria Wardani <vania.azria@ui.ac.id>
2022-09-21 17:19:24 +0700

    Film 9th Commit

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_html
 from mywatchlist.views import show_xml
 from mywatchlist.views import show_json
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('html/', show_mywatchlist, name='show_html'),
+    path('html/', show_html, name='show_html'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('', show_mywatchlist, name='show_mywatchlist'),
